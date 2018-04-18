9d1eea296426a12175c825958dd47d7b6b4318f2
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-18 20:00:29 +0100

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418161535) do
+ActiveRecord::Schema.define(version: 20180418184837) do
 
   create_table "posts", force: :cascade do |t|
     t.string "captation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
